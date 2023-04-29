659264bbe2a2173ae816b5387b6f51f961fa6909
shahiduljahid3 <shahiduljahid3@gmail.com>
2023-04-29 15:51:46 +0800

    first commit

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
 // data imports
-import User from "./models/User.js";
-import Product from "./models/Product.js";
-import ProductStat from "./models/ProductStat.js";
-import Transaction from "./models/Transaction.js";
-import OverallStat from "./models/OverallStat.js";
-import AffiliateStat from "./models/AffiliateStat.js";
-import {
-  dataUser,
-  dataProduct,
-  dataProductStat,
-  dataTransaction,
-  dataOverallStat,
-  dataAffiliateStat,
-} from "./data/index.js";
+// import User from "./models/User.js";
+// import Product from "./models/Product.js";
+// import ProductStat from "./models/ProductStat.js";
+// import Transaction from "./models/Transaction.js";
+// import OverallStat from "./models/OverallStat.js";
+// import AffiliateStat from "./models/AffiliateStat.js";
+// import {
+//   dataUser,
+//   dataProduct,
+//   dataProductStat,
+//   dataTransaction,
+//   dataOverallStat,
+//   dataAffiliateStat,
+// } from "./data/index.js";
 
 /* CONFIGURATION */
 dotenv.config();
@@ -62,3 +62,7 @@ mongoose
     // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
+
+  app.use("/", (req, res) => {
+    res.json({ mess: "iam still ALive" });
+  });
